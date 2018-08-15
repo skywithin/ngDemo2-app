221d35aecfd3f89bc71ec6c3e152d5fe0f7e5760
Max <sydneymax@gmail.com>
2018-08-15 22:32:32 +1000

    Add WarningAlert & SuccessAlert components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServerBucketComponent } from './server-bucket/server-bucket.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServerBucketComponent
+    ServerBucketComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
